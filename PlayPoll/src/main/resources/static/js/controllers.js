78ef460e22d18bca14a3eecf3ae06591ac5f3585
Boram <elmo1410@naver.com>
2014-10-09 22:10:25 +0900

    카카오톡 이미지테스트

--- PlayPoll/src/main/resources/static/js/controllers.js
+++ PlayPoll/src/main/resources/static/js/controllers.js
@@ -152,8 +152,7 @@ surveyControllers.controller('SendSurveyModalController', ['$scope', '$routePara
         container: '#kakao-link-btn',
         label: 'Any play poll! 설문에 참여해주세요',
         image: {
-         // src: 'http://dn.api1.kage.kakao.co.kr/14/dn/btqaWmFftyx/tBbQPH764Maw2R6IBhXd6K/o.jpg',
-            src: 'images/survey_3.png' ,
+            src: 'http://dn.api1.kage.kakao.co.kr/14/dn/btqaWmFftyx/tBbQPH764Maw2R6IBhXd6K/o.jpg',
             width: '300',
             height: '200'
         },
@@ -599,13 +598,13 @@ surveyControllers.controller('ReportController', [ '$scope', '$routeParams','$lo
             container: '#kakao-link-btn',
             label: 'Any play poll! 참여하신 설문의 결과 입니다',
             image: {
-              src:  'http://dn.api1.kage.kakao.co.kr/14/dn/btqaWmFftyx/tBbQPH764Maw2R6IBhXd6K/o.jpg',
+              src:  'images/poll_result.jpg' ,
               width: '300',
               height: '200'
             },
             webButton: {
              text: '설문 결과 보러가기',
-              url: 'http://dn.api1.kage.kakao.co.kr/14/dn/btqaWmFftyx/tBbQPH764Maw2R6IBhXd6K/o.jpg' // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
+              url: $scope.resultlink.link // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
             }
           });
         }
